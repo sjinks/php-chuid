dbaabbb6fb3eba088cc8d209a3123314d0c90bea
Volodymyr Kolesnykov <sjinks@users.noreply.github.com>
2016-08-01 07:41:49 +0300

    Fix setgroups() bug

--- helpers.c
+++ helpers.c
@@ -186,14 +186,15 @@ int set_guids(uid_t uid, gid_t gid TSRMLS_DC)
 	PHPCHUID_DEBUG("set_guids: mode=%d, uid=%d, gid=%d\n", (int)mode, (int)uid, (int)gid);
 
 	if (cxm_setresxid == mode || cxm_setxid == mode) {
+		res = setgroups(0, NULL);
+		if (0 != res) {
+			PHPCHUID_ERROR(E_CORE_WARNING, "Failed to clear the list of supplementary groups: %s", strerror(errno));
+		}
+
 		res = my_setgids(gid, gid, mode);
 		if (0 != res) {
 			PHPCHUID_ERROR(E_CORE_ERROR, "my_setgids(%d, %d, %d): %s", gid, gid, (int)mode, strerror(errno));
 			return FAILURE;
-		}
-
-		if (setgroups(0, NULL)) {
-			PHPCHUID_ERROR(E_CORE_WARNING, "Failed to clear the list of supplementary groups: %s", strerror(errno));
 		}
 	}
 
